271997edc57df046197ae37b5eb4b4b42edaca49
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2023-12-15 20:54:06 +0300

    contact page
    Editing on padding

--- app/contactme/page.js
+++ app/contactme/page.js
@@ -3,16 +3,17 @@ import Link from "next/link";
 export default function ContactMePage(){
     return (
         <div style={{padding:"20px"}}>
-            <h1 style={{paddingLeft:"30px"}}>Call me on:</h1>
-            <h2 style={{paddingLeft:"30px"}}>00962795372616</h2>
+            <h1 style={{paddingLeft:"30px", paddingRight:"30px"}}>Call me on:</h1>
+            <h2 style={{paddingLeft:"30px", paddingRight:"30px"}}>00962795372616</h2>
 
-            <h1 style={{paddingLeft:"30px"}}>Send me an email on:</h1>
-            <h2 style={{paddingLeft:"30px"}}>omaralkhanji@gmail.com</h2>
+            <h1 style={{paddingLeft:"30px", paddingRight:"30px"}}>Send me an email on:</h1>
+            <h2 style={{paddingLeft:"30px", paddingRight:"30px"}}>omaralkhanji@gmail.com</h2>
 
             <Link href="https://www.linkedin.com/in/omar-al-khanji-157a10b8/" 
-            style={{fontSize:"14pt",paddingLeft:"30px"}}>LinkedIn</Link>
+            style={{fontSize:"14pt",paddingLeft:"30px", paddingRight:"30px"}}>LinkedIn</Link>
+            <br/>
             <Link href="https://github.com/Omaralkhanjy94" 
-            style={{fontSize:"14pt",paddingLeft:"30px"}}>GetHub</Link>
+            style={{fontSize:"14pt",paddingLeft:"30px", paddingRight:"30px"}}>GetHub</Link>
         </div>
     );
 }
