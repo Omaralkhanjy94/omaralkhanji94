052f5720ac7f3644f739edf83bb9fb64a46a0b6f
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2023-12-15 21:14:08 +0300

    font size

--- app/contactme/page.js
+++ app/contactme/page.js
@@ -3,17 +3,21 @@ import Link from "next/link";
 export default function ContactMePage(){
     return (
         <div style={{padding:"20px"}}>
-            <h1 style={{paddingLeft:"30px", paddingRight:"30px"}}>Call me on:</h1>
-            <h2 style={{paddingLeft:"30px", paddingRight:"30px"}}>00962795372616</h2>
-
-            <h1 style={{paddingLeft:"30px", paddingRight:"30px"}}>Send me an email on:</h1>
-            <h2 style={{paddingLeft:"30px", paddingRight:"30px"}}>omaralkhanji@gmail.com</h2>
-
-            <Link href="https://www.linkedin.com/in/omar-al-khanji-157a10b8/" 
-            style={{fontSize:"14pt",paddingLeft:"30px", paddingRight:"30px"}}>LinkedIn</Link>
+            <p style={{paddingLeft:"10px", paddingRight:"30px"}}>Call me on:</p>
+            <p style={{paddingLeft:"20px", paddingRight:"30px"}}>00962795372616</p>
             <br/>
+            <p style={{paddingLeft:"10px", paddingRight:"30px"}}>Send me an email on:</p>
+            <p style={{paddingLeft:"20px", paddingRight:"30px"}}>omaralkhanji@gmail.com</p>
+            <br/>
+            <Link href="https://www.linkedin.com/in/omar-al-khanji-157a10b8/" 
+            style={{fontSize:"12pt",paddingLeft:"10px", 
+            paddingRight:"30px", backgroundColor:"lightBlue", 
+            color:"brown", margin:"10px", borderRadius: "10px", textAlign:"center"}}>LinkedIn</Link>
+            
             <Link href="https://github.com/Omaralkhanjy94" 
-            style={{fontSize:"14pt",paddingLeft:"30px", paddingRight:"30px"}}>GetHub</Link>
+            style={{fontSize:"12pt",paddingLeft:"10px", 
+            paddingRight:"30px", backgroundColor:"lightBlue", 
+            color:"brown", margin:"10px", borderRadius: "10px",textAlign:"center"}}>GetHub</Link>
         </div>
     );
 }
