8d67d6e1176ad3d5bc06b642d69f322bb2821d06
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2023-12-15 20:38:37 +0300

    about
    Editing on padding

--- app/aboutpage/page.js
+++ app/aboutpage/page.js
@@ -4,8 +4,8 @@ export default function AboutPage(){
     return (
         <div style={{padding:"20px"}}>
             <h1>About Page</h1>
-            <h2 >Hello, my name is Omar Al-Khanji</h2>
-                <p>
+            <h2 style={{padding:"30px"}}>Hello, my name is Omar Al-Khanji</h2>
+                <p style={{padding:"30px"}}>
                   I am software engineer from Jordan - Amman, and I make mobile & web applications.                  
                 </p>
         </div>
