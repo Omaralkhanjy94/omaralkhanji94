10bcad9db40c66caa51e9e4f5fa931d201b84b39
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2023-12-15 20:36:17 +0300

    about page
    adding padding on div

--- app/aboutpage/page.js
+++ app/aboutpage/page.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function AboutPage(){
     return (
-        <div>
+        <div style={{padding:"20px"}}>
             <h1>About Page</h1>
-            <h2>Hello, my name is Omar Al-Khanji</h2>
+            <h2 >Hello, my name is Omar Al-Khanji</h2>
                 <p>
                   I am software engineer from Jordan - Amman, and I make mobile & web applications.                  
                 </p>
