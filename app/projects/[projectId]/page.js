b73fed83aa038da18aff2d88cd13a57ee6f36b9e
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2023-12-16 21:06:15 +0300

    test23

--- app/projects/[projectId]/page.js
+++ app/projects/[projectId]/page.js
@@ -3,29 +3,27 @@ import ProjectDetails from "@/app/components/projectDetails";
 import { Suspense } from "react";
 import {promises as fs} from "fs";
 
-export default async function ProjectDetailsPage({params}){
+export default async function ProjectDetailsPage({params,projects}){
     //process.cwd() + 
     // const file = await fs.readFile(process.cwd() +`/public/json/data.json`, 'utf8');
     // const projects = JSON.parse(file);     
-    // const projectId = params.projectId;
+    const projectId = params.projectId;
 
-    // const projectJSX = projects.map((project)=>{
-    //     if(project.id === projectId)
-    //     {return <div key={projectId}>
-    //         <ProjectDetails project={project} projectId={projectId}/>
-    //     </div>}
-    //     else{
-    //         return <div key={projectId}> 
-    //     </div> 
-    //     }
-        
-
-    // },[]);
-    // const loadingJSX=(
-    //     <div>            
-    //         <p>Wait...</p>
-    //     </div>
-    // );
+    const projectJSX = projects.map((project)=>{
+        if(project.id === projectId)
+        {return <div key={projectId}>
+            <ProjectDetails project={project} projectId={projectId}/>
+        </div>}
+        else{
+            return <div key={projectId}> 
+        </div> 
+        }
+    },[]);
+    const loadingJSX=(
+        <div>            
+            <p>Wait...</p>
+        </div>
+    );
     return(
     <div style={{
         padding: "0px 0px 50px 30px",
