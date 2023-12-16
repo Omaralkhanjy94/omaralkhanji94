d4d58a05b722ba070b764fe5561aed65ab999a1b
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2023-12-16 22:58:54 +0300

    undo to editing on the path of data.json

--- app/projects/[projectId]/page.js
+++ app/projects/[projectId]/page.js
@@ -5,10 +5,10 @@ import { Suspense } from "react";
 import {promises as fs} from "fs";
 export default async function ProjectDetailsPage({params}){
     //process.cwd() + 
-    const file = await fs.readFile(`${process.cwd()}\\app\\api\\data.json`, 'utf8');
+    const file = await fs.readFile(`${process.cwd()}/app/api/data.json`, 'utf8');
     const projects = JSON.parse(file);     
     const projectId = params.projectId;
-    print(`${process.cwd()}\\app\\api\\data.json`);
+    print(`${process.cwd()}/app/api/data.json`);
     const projectJSX = projects.map((project)=>{
         if(project.id === projectId)
         {return <div key={projectId}>
