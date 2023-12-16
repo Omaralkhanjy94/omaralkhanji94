1fc89bb65e68d89fe69ed4aad717148ae7ac0742
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2023-12-16 22:33:38 +0300

    deleting logger.js and server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const logger = require('logger.js');
-
-// ... باقي التكوين
-
-// تسجيل بعض السجلات
-logger.info('This is an informational message');
-logger.error('This is an error message');
-
-// ... باقي الشيفرة
