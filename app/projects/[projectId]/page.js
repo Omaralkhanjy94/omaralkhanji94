e2a04bdfb1e0071e485fb859699d246e29e87df7
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2023-12-16 07:07:23 +0300

    15

--- app/projects/[projectId]/page.js
+++ app/projects/[projectId]/page.js
@@ -1,4 +1,4 @@
-'use server';
+'use client';
 
 import ProjectDetails from "@/app/components/projectDetails";
 // import print from "@/app/core";
@@ -6,6 +6,7 @@ import { Suspense } from "react";
 import {promises as fs} from "fs";
 export default async function ProjectDetailsPage({params}){
     //process.cwd() + 
+    
     const file = await fs.readFile(`${process.cwd()}/public/json/data.json`, 'utf8');
     const projects = JSON.parse(file);     
     const projectId = params.projectId;
