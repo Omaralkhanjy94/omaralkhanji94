a804aa9baf03979994deb5c8e36ca0f323d4f3c0
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2023-12-16 10:02:52 +0300

    yes

---
+++ server.js
@@ -0,0 +1,9 @@
+const logger = require('logger.js');
+
+// ... باقي التكوين
+
+// تسجيل بعض السجلات
+logger.info('This is an informational message');
+logger.error('This is an error message');
+
+// ... باقي الشيفرة
