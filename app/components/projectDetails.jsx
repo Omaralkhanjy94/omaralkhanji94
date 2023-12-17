b82eaff119d786fe498330f28287064588890dba
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2023-12-17 10:05:10 +0300

    Update projectDetails.jsx
    Removed some unimportant spaces.

--- app/components/projectDetails.jsx
+++ app/components/projectDetails.jsx
@@ -41,12 +41,13 @@ export default function ProjectDetails({projectId, project}){
         >
             <h3>{project.title}</h3>
             <hr/>
-            <div style={{padding:"25px 10px 5px 10px"} }>
-                <br/>
+            <div style={{padding:"25px 10px 10px 10px", marginLeft: "50px"} }>
+                
+                {/* <br/> */}
              {/* <i style={{color:`${messagecolor}`, fontSize:"12pt"}}>{message}</i>  */}
-             <br/><br/>
+             {/* <br/><br/> */}
              {/* <DownloadLink dl={`/public/files/proj8.apk`}/> */}
-             <br/><br/>
+             {/* <br/><br/> */}
              <h5>video</h5>
            
             <video className="projectvideo" src={project.video_path} width="100%" controls></video>
